ee153f86caf3ba1c188a735decda8e29f30fa750
marcia yurie yano <yurie_hikari@hotmail.com>
2021-08-27 10:17:10 -0300

    adicionando arquivo marcia.js

--- marcia.js
+++ marcia.js
@@ -1,8 +1,10 @@
-//--Perguntas para marcia--//
+/*Olá Meu nome é Marcia
+Tenho um cachorrinho chamado Chibi
+10x0 pro Git, já derreteu meu cerebro haha*/
+
+//--Repostas--//
 /*
-
-
-
+Final de semana eu gosto muito de tocar violão, ukulele ou teclado
 
 */
 
